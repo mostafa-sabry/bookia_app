fe44bf1d74e9e38b234851431760caaa7587da42
mostafa-sabry <engmostafasabryhassan@gmail.com>
2024-10-08 00:33:02 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
-import 'package:bookia_store/cache/cache_helper.dart';
 import 'package:bookia_store/cubit/book_cubit/books_cubit.dart';
 import 'package:bookia_store/cubit/cubit_book/cubit_wishlist/cubit/wishlist_cubit.dart';
 import 'package:bookia_store/cubit/cubit_cart/cubit/addcart_cubit.dart';
 import 'package:bookia_store/cubit/order/cubit/order_cubit.dart';
 
 import 'package:bookia_store/cubit/user_cubit/cubit/user_cubit.dart';
+import 'package:bookia_store/firebase_options.dart';
 import 'package:bookia_store/repositories/get_books_repo.dart';
 import 'package:bookia_store/repositories/get_order.dart';
 import 'package:bookia_store/repositories/user_repositories.dart';
 import 'package:bookia_store/ui/screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  CacheHelper().init();
-  runApp(
-    const MyApp(),
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
